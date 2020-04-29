348e519c158a29b2ad6a1a3a92a16b2a485f8151
Archibaald-dev <milo.lallement@yahoo.fr>
2020-04-29 17:12:21 +0200

    Add donation form, donation confirm, // hide form if not connected

--- App/View/main/home.php
+++ App/View/main/home.php
@@ -1,5 +1,6 @@
+<?php if (count($_POST)>0) echo "<div class='border-bottom border-success text-center'><p>La donation a bien été reçue !</p></div>"; ?>
 <div id="homePresentation">
-    <div id="homeContent1" class="container">  
+    <div id="homeContent1" class="container">
         <h1 class="text-white text-center my-5">Bienvenue sur le site de Animals</h1>
         <div id="homeText1">
             <div>
@@ -23,10 +24,10 @@
 <div id="homeAnimals">
     <div class="container">
         <h2 class="my-5 text-center">Les derniers animaux à adopter</h2>
-        <div class="row">   
-            <?php if($animals) { ?> 
-                <?php foreach($animals as $animal) { ?>
-                    <div class="col-md-4">  
+        <div class="row">
+            <?php if ($animals) { ?>
+                <?php foreach ($animals as $animal) { ?>
+                    <div class="col-md-4">
                         <div class="card">
                             <img class="card-img-top" src="https://fakeimg.pl/250x100/" alt="<?= $animal->getName() ?>">
                             <h5 class="card-title"><?= $animal->getName() ?></h5>
@@ -40,10 +41,10 @@
 <div id="homeProducts">
     <div class="container">
         <h2 class="my-5 text-center">Les derniers articles</h2>
-        <div class="row">   
-            <?php if($products) { ?> 
-                <?php foreach($products as $product) { ?>
-                    <div class="col-md-4">  
+        <div class="row">
+            <?php if ($products) { ?>
+                <?php foreach ($products as $product) { ?>
+                    <div class="col-md-4">
                         <div class="card">
                             <img class="card-img-top" src="https://fakeimg.pl/250x100/" alt="<?= $product->getName() ?>">
                             <h5 class="card-title"><?= $product->getName() ?></h5>
@@ -52,5 +53,34 @@
                 <?php } ?>
             <?php } ?>
         </div>
+    </div>
+</div>
+<div id="homeDonation">
+    <div class="container">
+
+        <?php //if (!isset($_SESSION["user"])): 
+        ?>
+
+        <h2 class="my-5 text-center">Se connecter pour faire une donation</h2>
+        <span class="navbar-text">
+            <a href="#" class="btn btn-outline-light mr-4">Se connecter</a>
+        </span>
+        <?php //else: 
+        ?>
+        <h2 class="my-5 text-center">Faire une donation</h2>
+        <div class="row">
+            <div class="col-md-5 text-center mx-auto">
+                <form action="index.php?page=home" method="POST">
+                    <div class="form-group">
+                        <label for="amount">Montant de la donation</label>
+                        <input type="number" required class="form-control" id="amount" name="amount" min="1" aria-describedby="amountHelp">
+                        <small id="amountHelp" class="form-text text-muted">Le montant de la donation est en euro €.</small>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Faire une donation</button>
+                </form>
+            </div>
+        </div>
+        <?php //endif 
+        ?>
     </div>
 </div>
