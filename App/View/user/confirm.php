24e508f4f30b043094032cb425b64ddb14953e4a
Nicolas <nicolas.brazzolotto@edu.devinci.fr>
2020-05-02 16:45:34 +0200

    Add command

---
+++ App/View/user/confirm.php
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="title text-center">
+                <h1>Votre Commande à été passé et est en cours de validation</h1>
+            </div>
+        </div>
+    </div>
+</div>
